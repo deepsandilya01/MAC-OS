e99001f46a8e0f90e5af2254a973385f1ce12526
deepsandilya01 <deepsandilya23@gamil.com>
2026-01-20 20:12:58 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useState } from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <main>
+      <Nav />
       <Dock />
     </main>
   );
