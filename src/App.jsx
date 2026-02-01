ac63520ca0388be0c61dde30042445592b5343c9
deepsandilya01 <deepsandilya23@gamil.com>
2026-02-01 19:59:14 +0530

    Add MacWindow component with draggable functionality and associated styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import "./app.scss";
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
+import MacWindow from "./windows/MacWindow";
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+
+      <MacWindow>
+        <h1>Hello</h1>
+      </MacWindow>
     </main>
   );
 };
